7e114571b428a777cbac24ea5b0b154a33408238
Atilla Çıtırık <114742494+AtillaCitirik@users.noreply.github.com>
2025-02-01 19:00:55 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PortalGroupCase
+//
+//  Created by Atilla Çıtırık on 13.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
